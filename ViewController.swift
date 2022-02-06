b8bd156812888babefe8313d52deec8f5b4b24f2
Jen-V <89663398+Jen-V@users.noreply.github.com>
2022-02-06 09:13:29 -0500

    Not sure what is happening here.
    Relating to github saved to my local folder. Is that the main that I commit to or am I commiting to the appbrewery file? I'll find out...

--- ViewController.swift
+++ ViewController.swift
@@ -24,5 +24,3 @@ imageView.image = randomBall
     }
      let ballArray = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball5"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball3"),]
     }
-
-
